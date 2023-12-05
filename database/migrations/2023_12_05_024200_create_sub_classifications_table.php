c9c73933b2bd60629aab82303d62f67dcb7c4eee
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-05 09:54:41 +0700

    database for superadmin

--- database/migrations/2023_12_05_024200_create_sub_classifications_table.php
+++ database/migrations/2023_12_05_024200_create_sub_classifications_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sub_classifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->foreignUuid('classification_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sub_classifications');
     }
 };
