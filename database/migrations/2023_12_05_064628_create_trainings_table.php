850a8ef65b6fa25d18181df4e6b180417638b6a0
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-05 13:25:32 +0700

    up database

---
+++ database/migrations/2023_12_05_064628_create_trainings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('dinas_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignUuid('fiscal_year_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignUuid('sub_classifications')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignUuid('qualification_level_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('name');
+            $table->dateTime('start_at');
+            $table->dateTime('end_time');
+            $table->integer('lesson_hour');
+            $table->text('location');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
