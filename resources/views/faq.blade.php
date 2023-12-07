00d91b1de5389b24cf5c9c600ed3164e52f8cd39
ibnzzi <muhammadibnzzi@gmail.com>
2023-12-07 13:54:13 +0700

    faq

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.app-landing-page')
+@section('content')
+        <div class="tabs-wrapper">
+            <div class="section-title text-center">
+                <h2 style="border-radius: 16px;
+                background: var(--Kuning, #FFC928);" class="title p-2">Frequently Asked Questions</h2>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane fade active show" id="design" role="tabpanel" aria-labelledby="design-tab">
+                    <div id="accordionsing" class="faq faq-two pixFade">
+                        <div class="card">
+                            <div class="card-header" id="heading10">
+                                <h5>
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse00" aria-expanded="false" aria-controls="collapse00">
+                                        Apa itu SIPJAKI ?
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse00" class="collapse" aria-labelledby="heading10" data-parent="#accordionsing" style="">
+                                <div class="card-body">
+                                    <p>
+                                        Easy peasy owt to do with me cras I don't want no agro what a load of rubbish starkers absolutely bladdered, old tinkety tonk old fruit so I said the full monty.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading20">
+                                <h5>
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse10" aria-expanded="false" aria-controls="collapse10">
+                                        Bagaimana kebijakan pengembalian?
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse10" class="collapse" aria-labelledby="heading20" data-parent="#accordionsing" style="">
+                                <div class="card-body">
+                                    <p>
+                                        Easy peasy owt to do with me cras I don't want no agro what a load of rubbish starkers absolutely bladdered, old tinkety tonk old fruit so I said the full monty.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading30">
+                                <h5>
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse20" aria-expanded="false" aria-controls="collapse20">
+                                        Bisakah saya menerima pengembalian uang?
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse20" class="collapse" aria-labelledby="heading30" data-parent="#accordionsing">
+                                <div class="card-body">
+                                    <p>
+                                        Easy peasy owt to do with me cras I don't want no agro what a load of rubbish starkers absolutely bladdered, old tinkety tonk old fruit so I said the full monty.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading40">
+                                <h5>
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse30" aria-expanded="false" aria-controls="collapse30">
+                                        Kapan saya akan menerima pesanan saya?
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse30" class="collapse" aria-labelledby="heading40" data-parent="#accordionsing">
+                                <div class="card-body">
+                                    <p>
+                                        Easy peasy owt to do with me cras I don't want no agro what a load of rubbish starkers absolutely bladdered, old tinkety tonk old fruit so I said the full monty.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading42">
+                                <h5>
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse32" aria-expanded="false" aria-controls="collapse32">
+                                        Apa yang harus saya lakukan jika saya tidak pernah menerima pesanan saya?
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse32" class="collapse" aria-labelledby="heading42" data-parent="#accordionsing">
+                                <div class="card-body">
+                                    <p>
+                                        Easy peasy owt to do with me cras I don't want no agro what a load of rubbish starkers absolutely bladdered, old tinkety tonk old fruit so I said the full monty.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
