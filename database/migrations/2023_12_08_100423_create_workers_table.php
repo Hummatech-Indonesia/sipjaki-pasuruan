e8d512fcde37c4234778da733a47ca791c3c39e7
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-08 10:23:09 +0700

    update

---
+++ database/migrations/2023_12_08_100423_create_workers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('service_provider_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('education');
+            $table->string('registration_number');
+            $table->string('cerificate'); // ganti enum jenis jenis sertifikat
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
