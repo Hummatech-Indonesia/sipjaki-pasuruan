f47deb5bd5a28eac663eb1635187bc651b3b83b2
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-11 10:06:19 +0700

    Update LoginService.php

--- app/Services/Auth/LoginService.php
+++ app/Services/Auth/LoginService.php
@@ -27,21 +27,22 @@ class LoginService
     public function handleLogin(LoginRequest $request)
     {
         if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
-            $this->historyLogin->store(['ip_address' => $request->ip()]);
-            
 
-            if ($request->is('api/*')) {
-                if (!auth()->user()->email_verified_at) {
-                    return ResponseHelper::error(null, trans('alert.account_unverified'), Response::HTTP_FORBIDDEN);
+            if(auth()->user()->email_verified_at){
+                $this->historyLogin->store(['ip_address' => $request->ip()]);
+                if ($request->is('api/*')) {
+                    $data['token'] =  auth()->user()->createToken('auth_token')->plainTextToken;
+                    $data['user'] = UserResource::make(auth()->user());
+                    return ResponseHelper::success($data, trans('alert.login_success'));
+                } else {
+                    return to_route('home');
                 }
-                $data['token'] =  auth()->user()->createToken('auth_token')->plainTextToken;
-                $data['user'] = UserResource::make(auth()->user());
-                return ResponseHelper::success($data, trans('alert.login_success'));
-            } else {
-                if (!auth()->user()->email_verified_at) {
+            }else{
+                if ($request->is('api/*')) {
+                    return ResponseHelper::error(null, trans('alert.account_unverified'), Response::HTTP_FORBIDDEN);
+                } else {
                     return redirect()->back()->withErrors(trans('alert.account_unverified'));
                 }
-                return to_route('home');
             }
         }
     }
