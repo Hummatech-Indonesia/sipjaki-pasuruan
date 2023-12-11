582920efa6871b1cada7b57d9c6580eb830a233e
paksiswanto <unitedkuryu@gmail.com>
2023-12-11 08:54:38 +0700

    fix bug

--- routes/daffa.php
+++ routes/daffa.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('history-login',function () {
     return view('pages.history-login');
 })->name('history-login.index');
+Route::get('history-login',function () {
+    return view('pages.history-login');
+})->name('history-login.index');
+
+Route::middleware(['role:admin'])->group(function () {
+    Route::get('agencies', [UserController::class, 'index'])->name('agencies.index');
+    Route::post('agencies', [UserController::class, 'store'])->name('agencies.store');
+    Route::put('agencies/{user}', [UserController::class, 'update'])->name('agencies.update');
+    Route::delete('agencies/{user}', [UserController::class, 'destroy'])->name('agencies.destroy');
+    Route::resources([
+        'news' => NewsController::class
+    ]);
+
+});
+
+
