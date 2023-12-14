bfa43da4edfbe3d509b0ca67dbfc294396fad21b
paksiswanto <unitedkuryu@gmail.com>
2023-12-14 08:32:20 +0700

    Update agency.blade.php

--- resources/views/pages/agency.blade.php
+++ resources/views/pages/agency.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 @section('content')
-@if(session('success'))
-<script>
-    Swal.fire({
-        icon: 'success',
-        title: 'Success',
-        text: '{{ session('success') }}',
-    });
-</script>
-@endif
+    @if (session('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: '{{ session('success') }}',
+            });
+        </script>
+    @endif
     <h2>Dinas</h2>
     <div class="d-flex justify-content-between">
         <div class="position-relative mb-3 col-lg-3">
@@ -23,7 +23,7 @@
         </div>
     </div>
     <div class="modal fade" id="modal-create" tabindex="-1" id="modal-create" aria-labelledby="exampleModalLabel1">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <form action="{{ route('agencies.store') }}" method="POST">
                     @csrf
@@ -34,20 +34,47 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="mb-3">
-                            <label id="name" for="recipient-name" class="control-label mb-2">Username</label>
-                            <input type="text" class="form-control" id="create-name" class="form-control" name="name"
-                                id="nametext" aria-describedby="name" placeholder="Masukan nama" />
-                        </div>
-                        <div class="mb-3">
-                            <label id="phone_number" for="recipient-name" class="control-label mb-2">Nomor Handphone</label>
-                            <input type="number" class="form-control" id="create-name" class="form-control" name="phone_number"
-                                id="nametext" aria-describedby="name" placeholder="Masukan nomor hp anda" />
-                        </div>
-                        <div class="mb-3">
-                            <label id="email" for="recipient-name" class="control-label mb-2">Email</label>
-                            <input type="email" class="form-control" id="create-email" class="form-control" name="email"
-                                aria-describedby="name" placeholder="Masukan email" />
+
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="mb-3">
+                                    <label id="name" for="recipient-name" class="control-label mb-2">Nama Dinas</label>
+                                    <input type="text" class="form-control" id="create-name" class="form-control"
+                                        name="name" id="nametext" aria-describedby="name" placeholder="Masukan nama" />
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="mb-3">
+                                    <label id="name" for="recipient-name" class="control-label mb-2">Penanggung
+                                        Jawab</label>
+                                    <input type="text" class="form-control" id="create-name" class="form-control"
+                                        name="person_responsible" id="nametext" aria-describedby="name"
+                                        placeholder="Masukan penanggung jawab" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="phone_number" for="recipient-name" class="control-label mb-2">Nomor
+                                        Handphone</label>
+                                    <input type="number" class="form-control" id="create-name" class="form-control"
+                                        name="phone_number" id="nametext" aria-describedby="name"
+                                        placeholder="Masukan nomor hp anda" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="email" for="recipient-name" class="control-label mb-2">Email</label>
+                                    <input type="email" class="form-control" id="create-email" class="form-control"
+                                        name="email" aria-describedby="name" placeholder="Masukan email" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="email" for="recipient-name" class="control-label mb-2">Password</label>
+                                    <input type="password" class="form-control" id="create-email" class="form-control"
+                                        name="password" aria-describedby="name" placeholder="Masukan password" />
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -63,21 +90,21 @@
             </div>
         </div>
     </div>
-     @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="row">
         @forelse ($users as $user)
             <div class="col-12 col-md-6 col-xl-4">
                 <div class="card">
                     <div class="card-body d-flex align-items-center">
-                        <img class="rounded-circle" src="{{ asset('assets/images/jobs.png') }}" alt=""
+                        <img class="rounded-circle" src="{{ $user->profile ?  asset('storage/'.$user->profile) : asset('Default.png') }}" alt=""
                             width="70" height="70" style="object-fit: cover;">
                         <div class="ms-3">
                             <h5 class="card-title">{{ $user->name }}</h5>
@@ -93,7 +120,8 @@
                                 <div class="col-6 mb-2">
                                     <button class="btn text-white btn-edit" id="btn-edit-{{ $user->id }}"
                                         data-id="{{ $user->id }}" data-name="{{ $user->name }}"
-                                        data-email="{{ $user->email }}" data-phone_number="{{ $user->phone_number }}" style="background-color: #1B3061; width: 100%;">
+                                        data-email="{{ $user->email }}" data-person_responsible="{{ $user->dinas->person_responsible }}" data-phone_number="{{ $user->phone_number }}"
+                                        style="background-color: #1B3061; width: 100%;">
                                         edit
                                     </button>
                                 </div>
@@ -116,12 +144,12 @@
                         <h4 class="text-center mt-4">Dinas Kosong!!</h4>
                     </div>
                 </div>
-            <div>
+                <div>
         @endforelse
     </div>
-    {{$users->links('pagination::bootstrap-5')}}
+    {{ $users->links('pagination::bootstrap-5') }}
     <div class="modal fade" id="modal-update" tabindex="-1" id="modal-create" aria-labelledby="exampleModalLabel1">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <form method="POST" id="form-update">
                     @csrf
@@ -133,31 +161,59 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="mb-3">
-                            <label id="name" for="recipient-name" class="control-label mb-2">Username</label>
-                            <input type="text" class="form-control" id="update-name" class="form-control" name="name"
-                                id="nametext" aria-describedby="name" placeholder="Masukan nama" />
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="mb-3">
+                                    <label id="name" for="recipient-name" class="control-label mb-2">Nama
+                                        Dinas</label>
+                                    <input type="text" class="form-control" id="update-name" class="form-control"
+                                        name="name" id="nametext" aria-describedby="name"
+                                        placeholder="Masukan nama" />
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="mb-3">
+                                    <label id="name" for="recipient-name" class="control-label mb-2">Penanggung
+                                        Jawab</label>
+                                    <input type="text" class="form-control" id="update-person_responsible" class="form-control"
+                                        name="person_responsible" id="nametext" aria-describedby="name"
+                                        placeholder="Masukan penanggung jawab" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="phone_number" for="recipient-name" class="control-label mb-2">Nomor
+                                        Handphone</label>
+                                    <input type="number" class="form-control" id="update-name" class="form-control"
+                                        name="phone_number" id="nametext" aria-describedby="name"
+                                        placeholder="Masukan nomor hp anda" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="email" for="recipient-name" class="control-label mb-2">Email</label>
+                                    <input type="email" class="form-control" id="update-email" class="form-control"
+                                        name="email" aria-describedby="name" placeholder="Masukan email" />
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="mb-3">
+                                    <label id="email" for="recipient-name"
+                                        class="control-label mb-2">Password</label>
+                                    <input type="password" class="form-control" id="update-email" class="form-control"
+                                        name="password" aria-describedby="name" placeholder="Masukan password" />
+                                </div>
+                            </div>
                         </div>
-                        <div class="mb-3">
-                            <label id="phone_number" for="recipient-name" class="control-label mb-2">Nomor Handphone</label>
-                            <input type="number" class="form-control" id="update-name" class="form-control" name="phone_number"
-                                id="nametext" aria-describedby="name" placeholder="Masukan nomor hp anda" />
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger text-white font-medium waves-effect"
+                                data-bs-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="submit" style="background-color: #1B3061" class="btn text-white btn-update">
+                                Edit
+                            </button>
                         </div>
-                        <div class="mb-3">
-                            <label id="email" for="recipient-name" class="control-label mb-2">Email</label>
-                            <input type="email" class="form-control" id="update-email" class="form-control" name="email"
-                                aria-describedby="name" placeholder="Masukan email" />
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger text-white font-medium waves-effect"
-                            data-bs-dismiss="modal">
-                            Close
-                        </button>
-                        <button type="submit" style="background-color: #1B3061" class="btn text-white btn-update">
-                            Edit
-                        </button>
-                    </div>
                 </form>
             </div>
         </div>
