ebd1eb908c5518552879b0c5a27a9e24e2345896
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-17 12:46:18 +0700

    Update 2023_12_15_210736_create_amendment_deeds_table.php

--- database/migrations/2023_12_15_210736_create_amendment_deeds_table.php
+++ database/migrations/2023_12_15_210736_create_amendment_deeds_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('city');
             $table->string('province');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
