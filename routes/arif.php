affbf67ab604bbf610b94b7dc9858a5307986232
mohamadarif03 <pecintarealmadrid2@gmail.com>
2023-12-18 18:11:17 +0700

    Update arif.php

--- routes/arif.php
+++ routes/arif.php
@@ -11,7 +11,8 @@ Route::put('dinas', [DinasController::class, 'update'])->name('dinas.update');
 Route::get('accident-chart', [DinasController::class, 'chart'])->name('accident.chart');
 Route::get('export-associations', [AssociationController::class, 'export']);
 
-Route::get('service-provider-qualifications', [ServiceProviderQualificationController::class, 'index']);
+
+Route::get('service-provider-qualifications', [ServiceProviderQualificationController::class, 'index'])->name('service.provider.qualifications');
 Route::post('service-provider-qualifications', [ServiceProviderQualificationController::class, 'store']);
 Route::put('service-provider-qualifications/{serviceProviderQualification}', [ServiceProviderQualificationController::class, 'update']);
 Route::patch('approve-service-provider-qualifications/{serviceProviderQualification}', [ServiceProviderQualificationController::class, 'approve']);
