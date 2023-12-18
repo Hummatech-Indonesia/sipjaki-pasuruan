bd46bc2697971c1195396233d45e7e7014e87e4a
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2023-12-18 13:56:09 +0700

    sub qualification

---
+++ app/Contracts/Repositories/SubQualificationRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Contracts\Repositories;
+
+use App\Contracts\Interfaces\SubQualificationInterface;
+use App\Models\SubQualification;
+
+class SubQualificationRepository extends BaseRepository implements SubQualificationInterface
+{
+    public function __construct(SubQualification $subQualification)
+    {
+        $this->model = $subQualification;
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function store(array $data): mixed
+    {
+        return $this->model->query()
+            ->create($data);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $id
+     * @return mixed
+     */
+    public function show(mixed $id): mixed
+    {
+        return $this->model->query()
+            ->where('qualification_id', $id)
+            ->get();
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $id
+     * @param  mixed $data
+     * @return mixed
+     */
+    public function update(mixed $id, array $data): mixed
+    {
+        return $this->model->query()
+            ->findOrFail($id)
+            ->update($data);
+    }
+
+    /**
+     * delete
+     *
+     * @param  mixed $id
+     * @return mixed
+     */
+    public function delete(mixed $id): mixed
+    {
+        return $this->model->query()
+            ->findOrFail($id)
+            ->delete();
+    }
+}
