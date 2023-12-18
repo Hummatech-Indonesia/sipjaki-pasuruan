df85061e57195734fac3b2d2e85e7696069cde65
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2023-12-18 11:08:23 +0700

    Dinas

--- app/Contracts/Interfaces/DinasInterface.php
+++ app/Contracts/Interfaces/DinasInterface.php
@@ -3,12 +3,13 @@
 namespace App\Contracts\Interfaces;
 
 use App\Contracts\Interfaces\Eloquent\CountInterface;
+use App\Contracts\Interfaces\Eloquent\CustomPaginationInterface;
 use App\Contracts\Interfaces\Eloquent\GetInterface;
 use App\Contracts\Interfaces\Eloquent\SearchInterface;
 use App\Contracts\Interfaces\Eloquent\ShowInterface;
 use App\Contracts\Interfaces\Eloquent\UpdateInterface;
 
-interface DinasInterface extends CountInterface,SearchInterface, UpdateInterface, ShowInterface, GetInterface
+interface DinasInterface extends CustomPaginationInterface,CountInterface,SearchInterface, UpdateInterface, ShowInterface, GetInterface
 {
    
 }
