0add616a1fdf69f96e9bc0f655ac5c0ead142604
ibnzzi <muhammadibnzzi@gmail.com>
2023-12-18 15:51:28 +0700

    training

--- resources/views/pages/classification/sub-training.blade.php
+++ resources/views/pages/classification/sub-training.blade.php
@@ -16,7 +16,7 @@
             </h2>
         </div>
         <div class="">
-            <a href="{{ route('classifications.index') }}" class="btn btn-warning">
+            <a href="{{ route('classification-training.index') }}" class="btn btn-warning">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 16" fill="none">
                     <path
                         d="M0.292893 7.29289C-0.0976314 7.68342 -0.0976315 8.31658 0.292892 8.7071L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34314C8.46159 1.95262 8.46159 1.31946 8.07107 0.928931C7.68054 0.538406 7.04738 0.538406 6.65686 0.928931L0.292893 7.29289ZM24 7L1 7L1 9L24 9L24 7Z"
@@ -149,6 +149,16 @@
                                 </td>
                             </tr>
                         @empty
+                        <tr>
+                            <td colspan="3" class="text-center">
+                                <div class="d-flex justify-content-center" style="min-height:16rem">
+                                    <div class="my-auto">
+                                        <img src="{{ asset('no-data.png') }}" width="300" height="300" />
+                                        <h4 class="text-center mt-4">Sub Klasifikasi Kosong!!</h4>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
                         @endforelse
 
                     </tbody>
