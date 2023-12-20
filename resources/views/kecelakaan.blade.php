2905451394ffbc099daee689eaab54a1b8fafc60
paksiswanto <unitedkuryu@gmail.com>
2023-12-20 16:12:00 +0700

    fix accident

--- resources/views/kecelakaan.blade.php
+++ resources/views/kecelakaan.blade.php
@@ -8,32 +8,82 @@
     <div class="tabs-wrapper">
         <div class="section-title text-center">
             <h2 style="border-radius: 16px;
-        background: var(--Kuning, #FFC928);" class="title p-1">Data Kecelakaan Kabupaten Pasuruan</h2>
+        background: var(--Kuning, #FFC928);" class="title p-1">Data Kecelakaan
+                Kabupaten Pasuruan</h2>
         </div>
     </div>
-    <div class="d-flex row">
-        <div class="col-xl-12">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Total Kecelakaan</h5>
-                    <h2>234 Korban</h2>
-                    <h5>Tahun 2023</h5>
-                    <div id="bar_chart" data-colors='["#1B3061"]' class="apex-charts" dir="ltr"></div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <div class="d-flex row">
         <div class="col-xl-12">
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title mb-4">Data Kecelakaan</h4>
-
-                    <div id="column_chart_datalabel" data-colors='["#FFC928"]' class="apex-charts" dir="ltr"></div>
+                    <form action="" class="row">
+                        <div class="col-4 d-flex justify-content-header gap-2">
+                            <select name="year" id="year" class="form-select mb-3">
+                            </select>
+                            <div class="">
+                                <button type="submit" class="text-white btn" style="background-color: #1B3061">
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <th class="fw-medium"
+                                    style="background-color: #1B3062; color: white; border-right: 1px solid #1B3061;">
+                                    No</th>
+                                <th class="fw-medium"
+                                    style="background-color: #1B3061; color: white; border-right: 1px solid #1B3061;">
+                                    Nama</th>
+                                <th class="fw-medium" style="background-color: #1B3061; color: white; text-align: center">
+                                    Jumlah Kecelakaan</th>
+                            </thead>
+                            <tbody>
+                                @forelse ($total as $data)
+                                    <tr>
+                                        <td>
+                                            {{ $loop->iteration }}
+                                        </td>
+                                        <td>
+                                            {{ $data->user->name }}
+                                        </td>
+                                        <td class="text-center">{{ $data->total_accident }}</td>
+                                    </tr>
+                                @empty
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
     <script src="assets/js/pages/apexcharts.init.js"></script>
+@endsection
+@section('script')
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var currentYear = new Date().getFullYear();
+
+            var startYear = 2023;
+
+            var yearSelectElement = document.getElementById("year");
+
+            var years = [];
+            for (var year = startYear; year <= currentYear; year++) {
+                years.push(year);
+            }
+
+            years.forEach(function(year) {
+                var optionElement = document.createElement("option");
+                optionElement.value = year;
+                optionElement.text = year;
+                yearSelectElement.add(optionElement);
+            });
+        });
+    </script>
 @endsection
