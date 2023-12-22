214d15d002c93fcfa03dbf73961b37e6459589a3
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2023-12-22 10:44:12 +0700

    update

--- resources/views/pages/detail-file-sercive.blade.php
+++ resources/views/pages/detail-file-sercive.blade.php
@@ -24,7 +24,7 @@
         <div id="carousel" class="carousel" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <canvas id="pdf-canvas" class="d-block w-100" data-file="{{ asset('storage/qualification/'.$serviceProviderQualification->file) }}"></canvas>
+                    <canvas id="pdf-canvas" class="d-block w-100" data-file="{{ asset('storage/'.$serviceProviderQualification->file) }}"></canvas>
                     <div class="carousel-caption d-none d-md-block">
                         <span>Page: <span id="page-num"></span> / <span id="page-count"></span></span>
                     </div>
