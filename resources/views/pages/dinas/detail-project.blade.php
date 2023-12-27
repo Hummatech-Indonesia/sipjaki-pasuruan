a02c5000a3eae8ed51766fe227ecdf9b56cf3054
Kader2637 <kadergantengrek@gmail.com>
2023-12-27 21:04:14 +0700

    Update detail-project.blade.php

--- resources/views/pages/dinas/detail-project.blade.php
+++ resources/views/pages/dinas/detail-project.blade.php
@@ -7,7 +7,7 @@
             </h3>
         </div>
         <div class="">
-            <a href="/projects"  class="btn text-white" style="background-color: #1B3061">
+            <a href="/projects" class="btn text-white" style="background-color: #1B3061">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 16" fill="none">
                     <path
                         d="M0.292892 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292892 7.29289ZM20 7L1 7V9L20 9V7Z"
@@ -47,7 +47,9 @@
                     <tr>
                         <td class="fw-bold">Status</td>
                         <td>:</td>
-                        <td id="detail-status">{{ $project->status == 'active' ? 'Aktif' : ($project->status == 'nonactive' ? 'Non-Aktif' : $project->status) }}</td>
+                        <td id="detail-status">
+                            {{ $project->status == 'active' ? 'Aktif' : ($project->status == 'nonactive' ? 'Non-Aktif' : $project->status) }}
+                        </td>
                     </tr>
                     <tr>
                         <td class="fw-bold">Konsultan</td>
@@ -95,10 +97,17 @@
                     <tr>
                         <td class="fw-bold">Karakteristik Kontrak</td>
                         <td>:</td>
-                        <td id="detail-characteristic_project_name">{{ $project->characteristic_project == 'single' ? 'Tahun Tunggal' : ($project->characteristic_project == 'multiple' ? 'Tahun Jamak' : $project->characteristic_project) }}</td>
+                        <td id="detail-characteristic_project_name">
+                            {{ $project->characteristic_project == 'single' ? 'Tahun Tunggal' : ($project->characteristic_project == 'multiple' ? 'Tahun Jamak' : $project->characteristic_project) }}
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        $('#projects').addClass('active');
+    </script>
 @endsection
