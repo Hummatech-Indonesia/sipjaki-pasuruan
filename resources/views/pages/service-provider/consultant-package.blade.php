ba830fac6f58acad23ec1bbbcbd6a3c3c46644fe
Kader2637 <kadergantengrek@gmail.com>
2023-12-27 19:31:32 +0700

    Update consultant-package.blade.php

--- resources/views/pages/service-provider/consultant-package.blade.php
+++ resources/views/pages/service-provider/consultant-package.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h4 class="mb-3 font-size-18">Paket Pekerjaan</h4>
-    <div class="col-12 col-lg-5 col-xxl-6 mb-3">
+    <div class="col-12 col-lg-5 col-xxl-4 mb-3">
         <form action="" class="">
             <div class="input-group d-flex ">
                 <input type="text" name="name" value="{{ request()->name }}" class="form-control" placeholder="Search">
