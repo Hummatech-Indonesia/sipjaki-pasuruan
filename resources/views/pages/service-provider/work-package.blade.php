226a139b48dffd0954e1b0b41c37345afbffc7cd
paksiswanto <unitedkuryu@gmail.com>
2024-01-05 13:43:42 +0700

    add button

--- resources/views/pages/service-provider/work-package.blade.php
+++ resources/views/pages/service-provider/work-package.blade.php
@@ -20,6 +20,14 @@
                 <div class="ms-2">
                     <button class="btn text-white" type="submit" style="background-color: #1B3061">Search</button>
                 </div>
+                <button class="btn btn-danger d-flex items-center gap-2">
+                    PDF<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="m23 12l-4-4v3h-9v2h9v3M1 18V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3h-2V6H3v12h12v-3h2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2Z"/></svg>
+                </i>
+                </button>
+                <button class="btn btn-success d-flex items-center gap-2">
+                    Excel<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="m23 12l-4-4v3h-9v2h9v3M1 18V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3h-2V6H3v12h12v-3h2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2Z"/></svg>
+                </i>
+                </button>
             </div>
         </form>
     </div>
