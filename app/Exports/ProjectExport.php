ce6f3929fc6adcec15a8bebae2c8014326164be1
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-01-08 11:29:30 +0700

    Project

--- app/Exports/ProjectExport.php
+++ app/Exports/ProjectExport.php
@@ -5,18 +5,20 @@ namespace App\Exports;
 use App\Models\Project;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request; // Import the Request class
+use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
 class ProjectExport implements FromView, ShouldAutoSize
 {
+    use Exportable;
+
     protected $request;
 
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
-
     /**
      * @return \Illuminate\Support\Collection
      */
