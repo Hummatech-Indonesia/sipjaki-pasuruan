fdb3124128e72d954463a41ad747f61eb3a5e8c5
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-01-23 23:27:11 +0700

    Update ServiceProviderProjectController.php

--- app/Http/Controllers/ServiceProviderProjectController.php
+++ app/Http/Controllers/ServiceProviderProjectController.php
@@ -112,10 +112,7 @@ class ServiceProviderProjectController extends Controller
      */
     public function show(ServiceProviderProject $service_provider_project)
     {
-        $projectYear = $service_provider_project->project->year;
-        $projectName = $service_provider_project->project->name;
         return ResponseHelper::success(ServiceProviderProjectResource::make($service_provider_project));
-        // return view('pages.service-provider.detail-progress', ['service_provider_project' => $service_provider_project]);
     }
 
     /**
