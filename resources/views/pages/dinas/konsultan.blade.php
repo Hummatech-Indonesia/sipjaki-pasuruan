92561552a96178c968fd73f4ec008bfb3e30703a
ibnzzi <muhammadibnzzi@gmail.com>
2024-02-15 11:55:29 +0700

    done

--- resources/views/pages/dinas/konsultan.blade.php
+++ resources/views/pages/dinas/konsultan.blade.php
@@ -13,7 +13,8 @@
                     <div class="">
                         <form action="" class="">
                             <div class="input-group">
-                                <input name="service_provider" type="text" class="form-control" placeholder="Search" value="{{ request()->service_provider }}">
+                                <input name="service_provider" type="text" class="form-control" placeholder="Search"
+                                    value="{{ request()->service_provider }}">
                                 <div class="input-group-append">
                                     <button class="btn text-white"
                                         style="background-color: #1B3061; border-radius: 0 5px 5px 0;" type="submit">
@@ -23,27 +24,29 @@
                             </div>
                         </form>
                     </div>
-                    <div class="">
-                        {{-- <button class="btn btn-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                fill="none">
-                                <path
-                                    d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-                                    stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M7 10L12 15L17 10" stroke="white" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M12 15V3" stroke="white" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                            </svg>
-                            Export
-                        </button> --}}
-                    </div>
+                </div>
+                <div class="d-flex">
+                    <button class="btn ms-1 text-white rounded" style="background-color:#1B3061" onclick="selectAll()">
+                        Pilih Semua
+                    </button>
+                    <form action="{{ route('delete-workers') }}" id="delete-multiple" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input type="hidden" name="worker_id" id="selected-worker">
+                        <button type="button" class="btn ms-1 text-white rounded" style="background-color:#E05C39"
+                            onclick="deleteSelected()">
+                            Hapus Pilihan
+                        </button>
+                    </form>
                 </div>
             </div>
             <div class="table-responsive mt-4">
                 <table class="table">
                     <thead>
                         <tr>
+                            <td scope="col" class="text-center text-white" style="background-color: #1B3061;">
+                                select
+                            </td>
                             <td class="text-white" style="background-color: #1B3061">
                                 No
                             </td>
@@ -67,6 +70,10 @@
                     @forelse ($serviceProviders as $serviceProvider)
                         <tbody>
                             <tr>
+                                <th scope="row" class="text-center">
+                                    <input value="{{ $serviceProvider->user->id }}" type="checkbox"
+                                        aria-label="Checkbox for following text input">
+                                </th>
                                 <td>
                                     {{ $loop->iteration }}
                                 </td>
@@ -84,26 +91,32 @@
 
                                 </td>
                                 <td>
-                                    <a href="detail-service-provider/{{ $serviceProvider->id }}" class="btn text-white"  style="background-color: #1B3061">
+                                    <a href="detail-service-provider/{{ $serviceProvider->id }}" class="btn text-white"
+                                        style="background-color: #1B3061">
                                         Detail
                                     </a>
+                                    <button type="button" id="{{ $serviceProvider->user->id }}"
+                                        data-id="{{ $serviceProvider->user->id }}"
+                                        data-name="{{ $serviceProvider->user->name }}" class="btn btn-danger btn-delete">
+                                        Hapus
+                                    </button>
                                 </td>
                             </tr>
                         </tbody>
                     @empty
-                    <tr>
-                        <td colspan="6" class="text-center">
-                            <div class="d-flex justify-content-center" style="min-height:16rem">
-                                <div class="my-auto">
-                                    <img src="{{ asset('no-data.png') }}" width="300" height="300" />
-                                    <h4 class="text-center mt-4">Konsultan Kosong!!</h4>
+                        <tr>
+                            <td colspan="6" class="text-center">
+                                <div class="d-flex justify-content-center" style="min-height:16rem">
+                                    <div class="my-auto">
+                                        <img src="{{ asset('no-data.png') }}" width="300" height="300" />
+                                        <h4 class="text-center mt-4">Konsultan Kosong!!</h4>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
                     @endforelse
                 </table>
-                {{$serviceProviders->links('pagination::bootstrap-5')}}
+                {{ $serviceProviders->links('pagination::bootstrap-5') }}
             </div>
         </div>
     </div>
@@ -218,15 +231,54 @@
             </div>
         </div>
     </div>
+    <div class="modal fade bs-example-modal-md" id="modal-reject" tabindex="-1" role="dialog"
+        aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color: #1B3061">
+                    <h5 class="modal-title text-white" id="myExtraLargeModalLabel">Hapus Akun</h5>
+                    <button type="button" class="btn-close" style="background-color: white" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
+                </div>
+                <form id="form-reject" method="post">
+                    <div class="modal-body">
+                        @csrf
+                        @method('PATCH')
+                        <h5 class="text-black" id="label">
+                        </h5>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="d-flex justify-content-end">
+                            <div class="d-flex justify-content-header gap-2">
+                                <div class="">
+                                    <button type="button" data-bs-dismiss="modal" class="btn btn-danger">Batal</button>
+                                </div>
+                                <div class="">
+                                    <button class="btn text-white" type="submit" style="background-color: #1B3061">
+                                        Hapus
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                @error('resend')
+                    <p>
+                        {{ $message }}
+                    </p>
+                @enderror
+            </div>
+        </div>
+    </div>
 @endsection
 @section('script')
-
     <script>
         $('.btn-detail').click(function() {
             id = $(this).data('id')
             console.log(id);
             get()
             $('#modal-detail').modal('show');
+
             function get() {
                 $.ajax({
                     url: "service-provider-detail/" + id,
@@ -246,37 +298,37 @@
                         var datawebsite = '';
                         if (website === null) {
                             var datawebsite = '-';
-                        }else{
+                        } else {
                             var datawebsite = website;
                         }
                         var datacity = '';
                         if (city === null) {
                             var datacity = '-';
-                        }else{
+                        } else {
                             var datacity = city;
                         }
                         var datapostal_code = '';
                         if (postal_code === null) {
                             var datapostal_code = '-';
-                        }else{
+                        } else {
                             var datapostal_code = city;
                         }
                         var dataprovince = '';
                         if (province === null) {
                             var dataprovince = '-';
-                        }else{
+                        } else {
                             var dataprovince = city;
                         }
                         var dataform_of_business_entity = '';
                         if (form_of_business_entity === null) {
                             var dataform_of_business_entity = '-';
-                        }else{
+                        } else {
                             var dataform_of_business_entity = city;
                         }
                         var datatype_of_business_entity = '';
                         if (type_of_business_entity === null) {
                             var datatype_of_business_entity = '-';
-                        }else{
+                        } else {
                             var datatype_of_business_entity = city;
                         }
                         $('#phone_number').text(phone_number)
@@ -293,5 +345,64 @@
                 });
             }
         })
+        $('.btn-delete').click(function() {
+            id = $(this).data('id')
+            name = $(this).data('name')
+            $('#label').html(`Apakah Anda Yakin Ingin Menghapus Akun Permanent dari ${name}?`);
+            var actionUrl = `reject-service-provider-qualifications/${id}`;
+            $('#form-reject').attr('action', actionUrl);
+            $('#modal-reject').modal('show')
+        })
+
+        function updateSelected() {
+            var selectedIds = [];
+            var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach(function(checkbox) {
+                if (checkbox.checked) {
+                    var workerId = checkbox.value;
+                    selectedIds.push(workerId);
+                }
+            });
+            document.getElementById('selected-worker').value = selectedIds;
+        }
+
+        function deleteSelected() {
+            updateSelected();
+            var selectedIds = document.getElementById('selected-worker').value;
+            if (selectedIds) {
+                Swal.fire({
+                    title: 'Konfirmasi',
+                    text: 'Apakah Anda yakin ingin melanjutkan?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonText: 'OK',
+                    cancelButtonText: 'Batal'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        document.getElementById('delete-multiple').submit();
+                    } else {
+                        Swal.fire('Dibatalkan', 'Aksi telah dibatalkan.', 'error');
+                    }
+                });
+            } else {
+                Swal.fire({
+                    'icon': 'error',
+                    'title': 'error',
+                    'text': 'Setidaknya Pilih Satu Akun Konsultan Untuk Dihapus'
+                })
+            }
+        }
+
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function(checkbox) {
+            checkbox.addEventListener('change', updateSelected);
+        });
+
+        function selectAll() {
+            var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            checkboxes.forEach(function(checkbox) {
+                checkbox.checked = true;
+            });
+        }
     </script>
 @endsection
