b87504e334dcd2507e578fc2136874c87f276d25
paksiswanto <unitedkuryu@gmail.com>
2024-06-27 10:41:49 +0700

    fix

--- resources/views/pages/download-manual-book.blade.php
+++ resources/views/pages/download-manual-book.blade.php
@@ -10,12 +10,30 @@
             </span>
         </button>
     </div>
-    <div class="d-flex justify-content-center mt-4">
-        <iframe id="detail-file" align="top" height="620" width="65%"
-        scrolling="auto" src ="{{ asset('Manual Book SIPJAKI.pdf') }}"></iframe>
+    <div class="container d-flex justify-content-center mt-4">
+        <main role="main" style="width:75%">
+            <div id="carousel" class="carousel" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <canvas id="pdf-canvas" class="d-block w-100"
+                            data-file="{{ asset('Manual Book SIPJAKI.pdf') }}"></canvas>
+                        <div class="carousel-caption d-none d-md-block">
+                            <span>Page: <span id="page-num"></span> / <span id="page-count"></span></span>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#" role="button" data-slide="prev">
+                    <i class="fas fa-chevron-left"></i>
+                </a>
+                <a class="carousel-control-next" href="#" role="button" data-slide="next">
+                    <i class="fas fa-chevron-right"></i>
+                </a>
+            </div>
+        </main>
     </div>
 @endsection
 @section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.min.js"></script>
     <script>
         function downloadManualBook() {
             var link = document.createElement('a');
@@ -23,5 +41,103 @@
             link.download = 'Manual Book SIPJAKI.pdf';
             link.click();
         }
+    </script>
+    <script>
+        
+       
+        $(function() {
+            let pdfDoc = null,
+                pageNum = 1,
+                pageRendering = false,
+                pageNumPending = null;
+
+            const scale = 5.0,
+                canvas = document.getElementById('pdf-canvas'),
+                pnum = document.getElementById('page-num'),
+                ctx = canvas.getContext('2d');
+
+            /**
+             * Get page info from document, resize canvas accordingly, and render page.
+             * @param num Page number.
+             */
+            function renderPage(num) {
+                pageRendering = true;
+
+                // Using promise to fetch the page
+                pdfDoc.getPage(num).then(function(page) {
+                    const page_viewport = page.getViewport({
+                        scale
+                    });
+                    canvas.height = page_viewport.height;
+                    canvas.width = page_viewport.width;
+
+                    // Render PDF page into canvas context
+                    const renderContext = {
+                        canvasContext: ctx,
+                        viewport: page_viewport
+                    };
+                    const renderTask = page.render(renderContext);
+
+                    // Wait for rendering to finish
+                    renderTask.promise.then(function() {
+                        pageRendering = false;
+                        if (pageNumPending !== null) {
+                            // New page rendering is pending
+                            renderPage(pageNumPending);
+                            pageNumPending = null;
+                        }
+                    });
+                });
+
+                // Update page counters
+                pnum.textContent = num;
+            }
+
+            /**
+             * If another page rendering in progress, waits until the rendering is
+             * finished. Otherwise, executes rendering immediately.
+             */
+            function queueRenderPage(num) {
+                if (pageRendering) {
+                    pageNumPending = num;
+                } else {
+                    renderPage(num);
+                }
+            }
+
+            /**
+             * Displays previous page.
+             */
+            document.querySelector(".carousel-control-prev").addEventListener('click', function() {
+                if (pageNum > 1) {
+                    pageNum--;
+                    queueRenderPage(pageNum);
+                }
+            });
+
+            /**
+             * Displays next page.
+             */
+            document.querySelector(".carousel-control-next").addEventListener('click', function() {
+                if (pageNum < pdfDoc.numPages) {
+                    pageNum++;
+                    queueRenderPage(pageNum);
+                }
+            });
+
+            /**
+             * Asynchronously downloads PDF.
+             */
+            (function() {
+                const url = canvas.dataset.file;
+                pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+                    pdfDoc = pdfDoc_;
+                    document.getElementById("page-count").textContent = pdfDoc.numPages;
+
+                    // Initial/first page rendering
+                    renderPage(pageNum);
+                });
+            })();
+        });
     </script>
 @endsection
