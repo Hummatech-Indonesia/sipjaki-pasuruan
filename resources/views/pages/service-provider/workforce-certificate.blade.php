2ee8886c205254622658ec21dbba507e7c92cc35
paksiswanto <unitedkuryu@gmail.com>
2024-06-27 13:47:11 +0700

    fix

--- resources/views/pages/service-provider/workforce-certificate.blade.php
+++ resources/views/pages/service-provider/workforce-certificate.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex justify-content-between">
         <h4 class="mb-3 font-size-18">Sertifikat</h4>
         <div class="">
-            <a href="/workers" class="btn me-2 btn-md btn-warning text-white cursor-pointer">
+            <a href="@role('superadmin|admin') {{ route('detail.service.provider',['service_provider' => $worker->serviceProvider->id]) }} @else {{route('workers.index')}}@endrole" class="btn me-2 btn-md btn-warning text-white cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 16" fill="none">
                     <path
                         d="M0.292893 7.29289C-0.0976314 7.68342 -0.0976315 8.31658 0.292892 8.7071L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34314C8.46159 1.95262 8.46159 1.31946 8.07107 0.928931C7.68054 0.538406 7.04738 0.538406 6.65686 0.928931L0.292893 7.29289ZM24 7L1 7L1 9L24 9L24 7Z"
