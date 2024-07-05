3a723595ac1101716b2a608f94049ecf6388ca26
putrapangestu <putraprp8@gmail.com>
2024-07-05 08:38:41 +0700

    MIGRATION ADD COLUMN
    commt add new column dinas_id in table executor_projects

--- database/migrations/2024_07_05_070852_add_column_dinas_id_on_table_executor_projects.php
+++ database/migrations/2024_07_05_070852_add_column_dinas_id_on_table_executor_projects.php
@@ -14,7 +14,7 @@ return new class extends Migration
         if(Schema::hasColumn('executor_projects', 'dinas_id')) return;
         Schema::table('executor_projects', function (Blueprint $table) {
             // Make the column nullable and reapply the constraints
-            $table->uuid('dinas_id')->nullable()->change();
+            $table->uuid('dinas_id')->nullable();
             $table->foreign('dinas_id')->references('id')->on('dinas')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
